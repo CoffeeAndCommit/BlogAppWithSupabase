a0287cbd6b1e2f88e66dd46e115d12cf5699b3fa
medhaviSharma11apr <medhavisharma11apr@gmail.com>
2025-05-10 18:41:58 +0530

    new commit

--- lib/features/auth/presentation/pages/signup_page.dart
+++ lib/features/auth/presentation/pages/signup_page.dart
@@ -11,57 +11,83 @@ class SignUpPage extends StatefulWidget {
 }
 
 class _SignUpPageState extends State<SignUpPage> {
+  final _formKey = GlobalKey<FormState>();
+  final _emailController = TextEditingController();
+  final _passwordController = TextEditingController();
+  final _nameController = TextEditingController();
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+    _emailController.dispose();
+    _passwordController.dispose();
+    _nameController.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         backgroundColor: AppColor.backgroundColor,
         body: Padding(
           padding: const EdgeInsets.all(8.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              const Text(
-                "Sign Up.",
-                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 50),
-              ),
-              const SizedBox(
-                height: 30,
-              ),
-              const AuthField(
-                hinttext: "Name",
-              ),
-              const SizedBox(
-                height: 15,
-              ),
-              const AuthField(
-                hinttext: "Email",
-              ),
-              const SizedBox(
-                height: 15,
-              ),
-              const AuthField(
-                hinttext: "Password",
-              ),
-              const SizedBox(
-                height: 15,
-              ),
-              const AuthGradientButton(onPressed: null),
-              const SizedBox(
-                height: 20,
-              ),
-              RichText(
-                  text: TextSpan(
-                      children: [
-                    TextSpan(
-                      text: 'Sign In',
-                      style: Theme.of(context).textTheme.titleMedium!.copyWith(
-                          color: AppColor.gradient2,
-                          fontWeight: FontWeight.bold),
-                    )
-                  ],
-                      text: "Don/'t have an account? ",
-                      style: Theme.of(context).textTheme.titleMedium))
-            ],
+          child: Form(
+            key: _formKey,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Text(
+                  "Sign Up.",
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 50),
+                ),
+                const SizedBox(
+                  height: 30,
+                ),
+                AuthField(
+                  controller: _nameController,
+                  hinttext: "Name",
+                ),
+                const SizedBox(
+                  height: 15,
+                ),
+                AuthField(
+                  controller: _emailController,
+                  hinttext: "Email",
+                ),
+                const SizedBox(
+                  height: 15,
+                ),
+                AuthField(
+                  isObscureText: true,
+                  controller: _passwordController,
+                  hinttext: "Password",
+                ),
+                const SizedBox(
+                  height: 15,
+                ),
+                AuthGradientButton(onPressed: (() {
+                  _formKey.currentState!.validate();
+                })),
+                const SizedBox(
+                  height: 20,
+                ),
+                RichText(
+                    text: TextSpan(
+                        children: [
+                      TextSpan(
+                        text: 'Sign In',
+                        style: Theme.of(context)
+                            .textTheme
+                            .titleMedium!
+                            .copyWith(
+                                color: AppColor.gradient2,
+                                fontWeight: FontWeight.bold),
+                      )
+                    ],
+                        text: "Don/'t have an account? ",
+                        style: Theme.of(context).textTheme.titleMedium))
+              ],
+            ),
           ),
         ));
   }
--- lib/features/auth/presentation/widgets/auth_field.dart
+++ lib/features/auth/presentation/widgets/auth_field.dart
@@ -1,19 +1,29 @@
 import 'package:flutter/material.dart';
 
 class AuthField extends StatelessWidget {
-  final String? Function(String?)? validator;
- final String? hinttext;
- const AuthField({
+  final String hinttext;
+  final bool isObscureText;
+  final TextEditingController controller;
+  const AuthField({
     super.key,
-    this.hinttext,
-    this.validator,
+    required this.hinttext,
+    required this.controller,
+    this.isObscureText = false,
   });
 
   @override
   Widget build(BuildContext context) {
     return TextFormField(
+      obscureText: isObscureText,
+      controller: controller,
       decoration: InputDecoration(hintText: hinttext),
-      validator: validator,
+      validator: ((value) {
+        if (value!.isEmpty) {
+          return "$hinttext can't be empty";
+        } else {
+          return "";
+        }
+      }),
     );
   }
 }
