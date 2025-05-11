a1a32f14a2fbc43b4b6193ecb3be7f747d1eb539
medhaviSharma11apr <medhavisharma11apr@gmail.com>
2025-05-11 17:22:25 +0530

    revert

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,13 @@
 import 'package:blog_app/core/routes/allRoutes.dart';
+import 'package:blog_app/core/secrets/app_secrets.dart';
 import 'package:blog_app/core/theme/theme.dart';
 import 'package:flutter/material.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
 void main() async {
   await Supabase.initialize(
-    url: 'https://etvrxeggvvshycfnctnq.supabase.co',
-    anonKey:
-        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImV0dnJ4ZWdndnZzaHljZm5jdG5xIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDY5NjMzNjAsImV4cCI6MjA2MjUzOTM2MH0.dvsxEiby5Q_UWPpjBdluw9lnwqFU9VHRhg-cagiUkJ0',
+    url: AppSecrets.supabaseUrl,
+    anonKey: AppSecrets.supabaseAnonKey,
   );
   runApp(const MyApp());
 }
