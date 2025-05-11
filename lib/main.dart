053156b68ea8ac6d0b36371bf24b2fe655a07bf4
medhaviSharma11apr <medhavisharma11apr@gmail.com>
2025-05-11 17:18:20 +0530

    supabase setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
 import 'package:blog_app/core/routes/allRoutes.dart';
 import 'package:blog_app/core/theme/theme.dart';
 import 'package:flutter/material.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
-void main() {
+void main() async {
+  await Supabase.initialize(
+    url: 'https://etvrxeggvvshycfnctnq.supabase.co',
+    anonKey:
+        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImV0dnJ4ZWdndnZzaHljZm5jdG5xIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDY5NjMzNjAsImV4cCI6MjA2MjUzOTM2MH0.dvsxEiby5Q_UWPpjBdluw9lnwqFU9VHRhg-cagiUkJ0',
+  );
   runApp(const MyApp());
 }
 
